bbc026936c2ab23c6c6c651dccba109631ce53a3
JordanRE <jordan.rob997@gmail.com>
2023-08-26 14:09:32 -0400

    Login interface avance
    UI estructurada (sin funcionalidad)

--- ProyectoQuest/Assets/Scripts/Managers/InterfaceManager.cs
+++ ProyectoQuest/Assets/Scripts/Managers/InterfaceManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InterfaceManager : MonoBehaviour
 {
     public static InterfaceManager instance;
+    public TMP_InputField inputName;
+    public TMP_InputField inputRut;
+    public TMP_InputField inputEmail;
+    public TMP_InputField inputPhone;
+
 
     private void Awake()
     {
