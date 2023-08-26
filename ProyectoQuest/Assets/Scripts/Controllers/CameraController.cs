7472372d576d2dbfbd79c30b4a190ab20d73d7ce
AukanYV <aukan.yv@gmail.com>
2023-08-26 01:29:49 -0400

    Camera controller funcional
    Funciona, falta pulir

--- ProyectoQuest/Assets/Scripts/Controllers/CameraController.cs
+++ ProyectoQuest/Assets/Scripts/Controllers/CameraController.cs
@@ -9,8 +9,14 @@ public class CameraController : MonoBehaviour
     public float deadZoneX = 5;
     public float deadZoneY = 7;
     
+    [Space(5)]
     public float offsetX = 0;
     public float offsetY = 0;
+
+    [Space(5)]
+    public float smoothingX = 0.3f;
+    public float smoothingY = 0.3f;
+
 
     [Space(20)]
     public Vector3 absolutePosition;
@@ -18,12 +24,21 @@ public class CameraController : MonoBehaviour
     public Vector3 cameraPosition;
 
     private Bounds bounds;
+    private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
     private float z;
 
+
+    private void Start()
+    {
+        absolutePosition = Vector3.zero;
+        relativePosition = Vector3.zero;
+        z = tCamera.transform.position.z;
+    }
     private void FixedUpdate()
     {
         Follow();
-        z = tCamera.transform.position.z;
+        
     }
 
     void Follow()
@@ -34,24 +49,53 @@ public class CameraController : MonoBehaviour
             return;
         }
 
-        bounds = new Bounds(target.position, Vector2.one);
+        bounds = new Bounds(target.position, new Vector3(0.1f, 0.1f, 0.1f));
+        bounds.Encapsulate(target.position);
+        offset = new Vector3(offsetX, offsetY, 0);
+        //Add other targets if multiplayer
 
-        absolutePosition = bounds.center;
-        relativePosition = absolutePosition + new Vector3(offsetX, offsetY, z);
-
-
+        //Out of bounds X axis
+        if (OutOfBounds(bounds.center.x, absolutePosition.x, deadZoneX))
+        {
+            //Suavizar
+            absolutePosition = new Vector3(bounds.center.x, absolutePosition.y, z);
+        }
+        //Out of bounds Y axis
+        if (OutOfBounds(bounds.center.y, absolutePosition.y, deadZoneY))
+        {
+            absolutePosition = new Vector3(absolutePosition.x, bounds.center.y, z);
+        }
+        
+        relativePosition = absolutePosition + offset;
+       
+        tCamera.transform.position = Vector3.SmoothDamp(tCamera.transform.position, relativePosition, ref velocity, smoothingX);
     }
 
-    public bool OutOfBounds()
+    public bool OutOfBounds(float targetPos, float relativePos, float deadZone)
     {
-        return true;
+        bool less = targetPos < relativePos - (deadZone / 2);
+        bool more = targetPos > relativePos + (deadZone / 2);
+        return less || more;
     }
 
     private void OnDrawGizmos()
     {
-        if (target == null) return;
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireCube(target.position, new Vector2(deadZoneX, deadZoneY));
+        Gizmos.color = Color.red;
+        if (target == null)
+        {
+            Gizmos.DrawWireCube(transform.position, new Vector2(deadZoneX, deadZoneY));
+        }
+        else
+        {
+            //Vector2 offset = new Vector2(offsetX, offsetY);
+            Gizmos.DrawWireCube(absolutePosition, new Vector2(deadZoneX, deadZoneY));
+            Gizmos.color = Color.yellow;    
+            Gizmos.DrawWireCube(relativePosition, new Vector2(deadZoneX, deadZoneY));
+            
+            Gizmos.color = Color.green;    
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
+            
+        }
     }
 
 }
