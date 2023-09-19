6caf16c9ea2a588e6aa06954babc07271d7a90ff
JordanRE <jordan.rob997@gmail.com>
2023-09-19 18:07:54 -0300

    Script emailer creado y funcional
    falta editarlo y adaptarlo para nuestro uso, pero funciona.

---
+++ ProyectoQuest/Assets/Scripts/Emailer/Emailer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Emailer : MonoBehaviour
+{
+    //[SerializeField] TMPro.TextMeshProUGUI txtData;
+    [SerializeField] UnityEngine.UI.Button btnSubmit;
+    [SerializeField] bool sendDirect = true;
+
+    const string kSenderEmailAddress = "jordan.rob997@gmail.com";
+    const string kSenderPassword = "zdkz vkpe qiah hkdb";
+    const string kReceiverEmailAddress = "jordan.rob997@gmail.com";
+
+    void Start()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull("---hola---");
+        UnityEngine.Assertions.Assert.IsNotNull(btnSubmit);
+        btnSubmit.onClick.AddListener(delegate {
+            if (sendDirect)
+            {
+                SendAnEmail("---hola---");
+            }
+        });
+    }
+
+    // Method 1: Direct message
+    private static void SendAnEmail(string message)
+    {
+        // Create mail
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress(kSenderEmailAddress);
+        mail.To.Add(kReceiverEmailAddress);
+        mail.Subject = "Email Title";
+        mail.Body = message;
+
+        // Setup server 
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new NetworkCredential(
+            kSenderEmailAddress, kSenderPassword) as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate,
+            X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+                Debug.Log("Email success!");
+                return true;
+            };
+
+        // Send mail to server, print results
+        try
+        {
+            smtpServer.Send(mail);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("Email error: " + e.Message);
+        }
+        finally
+        {
+            Debug.Log("Email sent!");
+        }
+    }
+}
