402c151c322406318b11659adb12036c30ccf833
iKoDaesu <ikodiazv@gmail.com>
2023-10-06 13:57:10 -0300

    Agregar solo movimiento con animaciones

--- ProyectoQuest/Assets/Scripts/Player.cs
+++ ProyectoQuest/Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
     private float horizontal;
     private float vertical;
     private bool canMove = true;
+    int direction;
 
     private void Start()
     {
@@ -50,13 +51,11 @@ public class Player : MonoBehaviour
 
         if (target.rigidBody.velocity.x != 0 || target.rigidBody.velocity.y != 0)
         {
-            //Debug.Log("Vertical: " + target.rigidBody.velocity.y);
-            //Debug.Log("Horizontal: " + target.rigidBody.velocity.x);
-            if (target.rigidBody.velocity.y > 0) anim[GameManager.instance.characterIndex].SetTrigger("Up");
-            if (target.rigidBody.velocity.x < 0) anim[GameManager.instance.characterIndex].SetTrigger("Left");
-            if (target.rigidBody.velocity.x > 0) anim[GameManager.instance.characterIndex].SetTrigger("Right");
-            if (target.rigidBody.velocity.y < 0) anim[GameManager.instance.characterIndex].SetTrigger("Down");
+            DirectionAnim();
+            SetAnimation();
         }
+               //Debug.Log("Vertical: " + target.rigidBody.velocity.y);
+            //Debug.Log("Horizontal: " + target.rigidBody.velocity.x);
         //bool idle = (horizontal == 0 && vertical == 0) ? true : false;
         anim[GameManager.instance.characterIndex].SetBool("Idle", (target.rigidBody.velocity.x == 0 && target.rigidBody.velocity.y == 0) ? true : false);
         //if (horizontal == 0 && vertical == 0) anim[GameManager.instance.characterIndex].SetBool("Idle", true);
@@ -70,4 +69,57 @@ public class Player : MonoBehaviour
     {
         return canMove;
     }
+
+    public void DirectionAnim()
+    {
+
+        if (target.rigidBody.velocity.y > 0)
+        {
+            if(direction != 1)
+            {
+                Debug.Log("1");
+                direction = 1;
+                anim[GameManager.instance.characterIndex].SetTrigger("Up");
+                return;
+            }
+        }
+        else if (target.rigidBody.velocity.y < 0) 
+        {
+            if (direction != 4)
+            {
+                Debug.Log("4");
+                direction = 4;
+                anim[GameManager.instance.characterIndex].SetTrigger("Down");
+                return;
+            }
+        }
+                
+        else if (target.rigidBody.velocity.x < 0) 
+        {
+            if (direction != 2)
+            {
+                Debug.Log("2");
+                direction = 2;
+                anim[GameManager.instance.characterIndex].SetTrigger("Left");
+                return;
+            }
+        }
+        else if (target.rigidBody.velocity.x > 0)
+        {
+            if (direction != 3)
+            {
+                Debug.Log("3");
+                direction = 3;
+                anim[GameManager.instance.characterIndex].SetTrigger("Right");
+                return;
+            }
+        }
+    }
+
+    void SetAnimation()
+    {
+
+    }
+
 }
+
